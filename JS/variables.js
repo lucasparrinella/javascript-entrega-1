3ae1cc962311c03672ff23a237761c8baf9cdec0
lucasparrinella <88515708+lucasparrinella@users.noreply.github.com>
2021-10-26 19:39:25 -0300

    Desafío 1

---
+++ JS/variables.js
@@ -0,0 +1,6 @@
+let nombreUsuario = prompt("¿Cuál es tu nombre?");
+let edadUsuario = Number(prompt("¿Cuántos años tenés? (Sólos números validos)"));
+const edad10 = 10 + edadUsuario; 
+
+alert("Bienvenido " + nombreUsuario +" ¡Esperamos que tengas una buena estadia!")
+alert("Tu edad nos parece más que suficiente para que ingreses, siendo esta misma de " + edadUsuario + " años. Dentro de 10 años tendrás " + edad10 + " años");
